4fa2636fbd98f6e91b91ec747c44f96943bcf5a4
Turkxy Tech <159791470+turkxytech@users.noreply.github.com>
2024-02-21 02:19:38 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function colorTrace(msg, color) {
+    console.log("%c" + msg, "color:" + color + ";font-weight:bold;" + ";font-size:50px;" + ";text-align:center;");
+    }
+    colorTrace("WARNING", "red");
+
+    console.warn("This isn't the offical website");
+    console.warn("This is a remake made by TurkxyTech");
+    console.warn("All rights preserved by Blender Inc");
+    console.warn("All rights preserved by TurkxyTech");
